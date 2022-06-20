10b5ae495bf307cb1bf472c475c77980114637e5
Nachoooooooo <107418289+Nachoooooooo@users.noreply.github.com>
2022-06-20 19:51:36 +0200

    <tu-mensaje>

---
+++ index.js
@@ -0,0 +1,31 @@
+
+/* Ejercicio 11*/
+var postI = 0;
+var postJ = 0;
+for (let i = 0; i < 11; i++) {
+    postI+=postJ++   
+}  
+
+/* Ejercicio 12*/
+var sumaPares = 0;
+for (let i = 0; i < 10; i++) {
+    if (i%2 == 0) {
+     sumaPares += i   
+    }
+    
+}
+/* Ejercicio 24*/
+var contarHasta10 = 0;
+do {
+    contarHasta10++
+} 
+while (contarHasta10 < 10);
+
+/* Ejercicio 24*/localStorage
+var sumaImpares = 0;
+for (let i = 0; i < 10; i++) {
+    if (i%2 !=0) {
+        sumaImpares += i   
+       } 
+
+}
